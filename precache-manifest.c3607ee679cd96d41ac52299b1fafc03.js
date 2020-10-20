fa9adeb0f9e3156cd09832e472ba2d5d3d268a73
Gera <sanchez2014@gmail.com>
2020-10-20 00:29:51 -0500

    Updates

---
+++ precache-manifest.c3607ee679cd96d41ac52299b1fafc03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493b2ae317aca3f0765538d5036e6ea3",
+    "url": "/Administrador_citas/index.html"
+  },
+  {
+    "revision": "24ea76062c9f54dc34cd",
+    "url": "/Administrador_citas/static/css/main.258de1d6.chunk.css"
+  },
+  {
+    "revision": "ba7bf88f4656c6303172",
+    "url": "/Administrador_citas/static/js/2.d01a3f8a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Administrador_citas/static/js/2.d01a3f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ea76062c9f54dc34cd",
+    "url": "/Administrador_citas/static/js/main.c6503df3.chunk.js"
+  },
+  {
+    "revision": "556adfb575faad5dbba6",
+    "url": "/Administrador_citas/static/js/runtime-main.7dfcdb32.js"
+  }
+]);
